921136070ff8737fcf2a07902ef29ff50b594d1e
“Chantel <chmagee@ucsc.edu>
2021-04-08 15:58:28 -0700

    broken starfield bg

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -2,8 +2,15 @@ class Play extends Phaser.Scene {
     constructor() {
         super("playScene");
     }
+    
+    preload() {
+        this.load.image('starfield', 'assets/starfield.png');
+    }
 
     create() {
+        //starfield
+        this.starfield = this.add.tileSprite(0,0,640,480, 'starfield').setOrigin(0,0);
+
        // green UI bg
        this.add.rectangle(0, borderUISize + borderPadding, 
         game.config.width, borderUISize * 2, 0x00FF00,).setOrigin(0,0);
@@ -13,6 +20,6 @@ class Play extends Phaser.Scene {
         this.add.rectangle(0, game.config.height - borderUISize, game.config.width, borderUISize, 0xFFFFFF).setOrigin(0 ,0);
         this.add.rectangle(0, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0 ,0);
         this.add.rectangle(game.config.width - borderUISize, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0 ,0);
-
+        
     }
 }
