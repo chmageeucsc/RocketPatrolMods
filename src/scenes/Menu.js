48d8d02433bb6fdc5ad54df8fda5aeeb132e3655
“Chantel <chmagee@ucsc.edu>
2021-04-10 15:58:28 -0700

    typo fix

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -30,7 +30,7 @@ class Menu extends Phaser.Scene {
 
         //show menu text
         this.add.text(game.config.width / 2, game.config.height / 2 - borderUISize -
-            borderPadding, 'ROCKEY PATROL', menuConfig).setOrigin(0.5);
+            borderPadding, 'ROCKET PATROL', menuConfig).setOrigin(0.5);
         this.add.text(game.config.width / 2, game.config.height / 2 , 
             'Use <- -> arrows to move & (F) to fire', menuConfig).setOrigin(0.5);
         menuConfig.backgroundColor = '#00FF00';
